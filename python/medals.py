10cbc2522f31257d9ce0dadc7e66a17b08db6a37
Richard Parle <richardparle@Richards-MacBook-Air.local>
2022-06-09 08:41:34 +0100

    complete python tech test

--- python/medals.py
+++ python/medals.py
@@ -17,14 +17,54 @@ medalResults = [
     }
 ]
 
+
 def createMedalTable(results):
-    # Use the results object above to create a medal table
-    # The winner gets 3 points, second place 2 points and third place 1 point
-    return
+
+    medal_table = {}
+
+    medalists = []
+    first_place = []
+    second_place = []
+    third_place = []
+
+    # Place each medalist into "medalists" array
+    for event in results:
+        for country in event['podium']:
+            medalists.append(country)
+
+    # Seperate 1st, 2nd and 3rd into seperate arrays and remove first 2 characters
+    for medalist in medalists:
+        if medalist.startswith('1'):
+            first_place.append(medalist[2: len(medalist)])
+        if medalist.startswith('2'):
+            second_place.append(medalist[2: len(medalist)])
+        if medalist.startswith('3'):
+            third_place.append(medalist[2: len(medalist)])
+
+    # Check if country exists in table, if it does then increament the value by the relavent amount, if not then add it.
+    for country in first_place:
+        if country in medal_table:
+            medal_table[country] += 3
+        else:
+            medal_table[country] = 3
+
+    for country in second_place:
+        if country in medal_table:
+            medal_table[country] += 2
+        else:
+            medal_table[country] = 2
+
+    for country in third_place:
+        if country in medal_table:
+            medal_table[country] += 1
+        else:
+            medal_table[country] = 1
+
+    return medal_table
 
 
 def test_function():
-    #This it the test function, please don't change me
+    # This it the test function, please don't change me
     medalTable = createMedalTable(medalResults)
     expectedTable = {
         "Italy": 4,
